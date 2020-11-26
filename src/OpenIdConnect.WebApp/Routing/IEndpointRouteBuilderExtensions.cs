c90a383a94c76252163bf194cc17770d7cea918e
Jörg Jooss (MSFT) <joerg.jooss@microsoft.com>
2020-11-26 13:49:02 +0100

    fix: move extension methods to Routing namespace

--- src/OpenIdConnect.WebApp/Routing/IEndpointRouteBuilderExtensions.cs
+++ src/OpenIdConnect.WebApp/Routing/IEndpointRouteBuilderExtensions.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 
-namespace OpenIdConnect.WebApp
+namespace OpenIdConnect.WebApp.Routing
 {
     public static class IEndpointRouteBuilderExtensions
     {
--- src/OpenIdConnect.WebApp/Startup.cs
+++ src/OpenIdConnect.WebApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.HttpOverrides;
+using OpenIdConnect.WebApp.Routing;
 
 namespace OpenIdConnect.WebApp
 {
